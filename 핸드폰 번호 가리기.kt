5b8dc136e5acd6363af8ce025f152a7c678872b7
npureaun <98468118+npureaun@users.noreply.github.com>
2024-09-26 12:26:08 +0900

    Update 핸드폰 번호 가리기.kt

--- 핸드폰 번호 가리기.kt
+++ 핸드폰 번호 가리기.kt
@@ -1,7 +1,7 @@
-class Solution2 {
+class Solution {
     fun solution(phone_number: String): String {
         var answer = StringBuilder(phone_number)
-        (0..answer.length-5).forEach{
+        (0 until answer.length-4).forEach{
             answer[it]='*'
         }
         return answer.toString()
